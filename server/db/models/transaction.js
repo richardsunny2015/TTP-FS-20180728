ad9def4ffa7e6bfbe458720ea955bc9c9b093b5d
richardsunny2015 <richardsunny2015@gmail.com>
2018-10-24 17:51:48 -0400

    ci(transaction): Create transaction table for db

---
+++ server/db/models/transaction.js
@@ -0,0 +1,44 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Transaction = db.define('transaction', {
+  type: {
+    type: Sequelize.ENUM('sale', 'purchase'),
+    allowNull: false
+  },
+  userId: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  shares: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+  },
+  pricePerShare: {
+      type: Sequelize.FLOAT,
+      allowNull: false
+  },
+  total: {
+      type: Sequelize.VIRTUAL,
+      get() {
+          return this.getDataValue('pricePerShare') * this.getDataValue('shares')
+      }
+  }
+})
+
+module.exports = Transaction
+
+/**
+ * instanceMethods
+ */
+
+
+/**
+ * classMethods
+ */
+
+
+/**
+ * hooks
+ */
+
