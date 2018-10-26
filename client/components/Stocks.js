483d43beea55fe1a6d99b197c673894fe8bcf780
richardsunny2015 <richardsunny2015@gmail.com>
2018-10-26 12:39:22 -0400

    feat(Stocks): Prevent users from buying more than they can afford

--- client/components/Stocks.js
+++ client/components/Stocks.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 import axios from 'axios'
 
 const Stocks = props => {
-  const {stocks, userId, shares} = props
+  const {stocks, userId, shares, balance} = props
   const isPortfolio = props.isPortfolio || false
 
   return (
@@ -12,8 +12,8 @@ const Stocks = props => {
         <div key={stock.symbol}>
           <h4>{stock.symbol}</h4>
           <p>Current Price: {stock.price}</p>
-          {isPortfolio && (<p># of Shares: {shares[idx]}</p>)}
-          <form onSubmit={purchaseSubmit(stock, userId)}>
+          {isPortfolio && <p># of Shares: {shares[idx]}</p>}
+          <form onSubmit={purchaseSubmit(stock, userId, balance)}>
             <input type="number" name="shares" min="1" />
             <button type="submit" name="action" value="purchase">
               Buy
@@ -25,27 +25,32 @@ const Stocks = props => {
   )
 }
 
-const purchaseSubmit = (stockInfo, userId) => evt => {
+const purchaseSubmit = (stockInfo, userId, balance) => evt => {
   evt.preventDefault()
   const shares = Number(evt.target.shares.value)
-  const objToSend = {
-    shares,
-    pricePerShare: stockInfo.price,
-    type: 'purchase',
-    symbol: stockInfo.symbol,
-    userId
+  const total = stockInfo.price * shares
+  if (total < balance) {
+    const objToSend = {
+      shares,
+      pricePerShare: stockInfo.price,
+      type: 'purchase',
+      symbol: stockInfo.symbol,
+      userId
+    }
+    axios
+      .post('/api/transactions', objToSend)
+      .then(res => res.data)
+      .then(console.log)
+      .catch(console.log)
+  } else {
+    console.log('TOO MUCH')
   }
-  console.log(objToSend, 'our obj')
-  axios
-    .post('/api/transactions', objToSend)
-    .then(res => res.data)
-    .then(console.log)
-    .catch(console.log)
 }
 
 const mapStateToProps = state => ({
   userId: state.user.id,
-  shares: state.portfolio.map(portfolio => portfolio.shares)
+  shares: state.portfolio.map(portfolio => portfolio.shares),
+  balance: state.user.balance
 })
 
 export default connect(mapStateToProps)(Stocks)
