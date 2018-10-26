f63e594aec3f0458005bd5219671b53377c5e65d
richardsunny2015 <richardsunny2015@gmail.com>
2018-10-26 09:07:41 -0400

    fix(transactions): Secure transactions route

--- server/api/transactions.js
+++ server/api/transactions.js
@@ -4,24 +4,30 @@ module.exports = router
 
 router.post('/', async (req, res, next) => {
   const {pricePerShare, shares, userId, type, symbol} = req.body
-  try {
-    const transaction = await Transaction.create({
-      pricePerShare,
-      shares,
-      type,
-      symbol
-    })
-    const user = await User.findById(userId)
-    transaction.setUser(user)
-    user.updateBalance(transaction)
-    const portfolio = await Portfolio.findOrCreate({where: {symbol: transaction.symbol, userId}})
-    portfolio.updateShares(transaction)
-    if (portfolio.shares === 0) await portfolio.destroy()
-    else await portfolio.save()
-    await user.save()
-    await transaction.save()
-    res.status(201).send(transaction)
-  } catch (err) {
-    next(err)
+  if (req.user.id !== userId) {
+    try {
+      const transaction = await Transaction.create({
+        pricePerShare,
+        shares,
+        type,
+        symbol
+      })
+      const user = await User.findById(userId)
+      transaction.setUser(user)
+      user.updateBalance(transaction)
+      const portfolio = await Portfolio.findOrCreate({
+        where: {symbol: transaction.symbol, userId}
+      })
+      portfolio.updateShares(transaction)
+      if (portfolio.shares === 0) await portfolio.destroy()
+      else await portfolio.save()
+      await user.save()
+      await transaction.save()
+      res.status(201).send(transaction)
+    } catch (err) {
+      next(err)
+    }
+  } else {
+    res.status(403).send('Forbidden')
   }
 })
