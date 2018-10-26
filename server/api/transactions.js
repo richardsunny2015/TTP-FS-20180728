c65d8d07b4290341734eab5577aa0304c17a6c81
Richard Lanchez <richardsunny2015@gmail.com>
2018-10-26 09:57:35 -0400

    Portfolio route #15 (#25)
    * test(portfolio): Test updateShares instance method
    
    * feat(transactions): Transactions post route finds or creates Portfolio instance
    
    * fix(transactions): Secure transactions route
    
    * fix(transactions): Change initial portfolio to portfolioArray
    
    * feat(users): Add get route for a user's portfolio

--- server/api/transactions.js
+++ server/api/transactions.js
@@ -1,23 +1,34 @@
 const router = require('express').Router()
-const {Transaction, User} = require('../db/models')
+const {Transaction, User, Portfolio} = require('../db/models')
 module.exports = router
 
 router.post('/', async (req, res, next) => {
   const {pricePerShare, shares, userId, type, symbol} = req.body
-  try {
-    const transaction = await Transaction.create({
-      pricePerShare,
-      shares,
-      type,
-      symbol
-    })
-    const user = await User.findById(userId)
-    transaction.setUser(user)
-    user.updateBalance(transaction)
-    await user.save()
-    await transaction.save()
-    res.status(201).send(transaction)
-  } catch (err) {
-    next(err)
+  if (req.user.id === userId) {
+    try {
+      const transaction = await Transaction.create({
+        pricePerShare,
+        shares,
+        type,
+        symbol
+      })
+      const user = await User.findById(userId)
+      transaction.setUser(user)
+      user.updateBalance(transaction)
+      const portfolioArray = await Portfolio.findOrCreate({
+        where: {symbol: transaction.symbol, userId}
+      })
+      const portfolio = portfolioArray[0]
+      portfolio.updateShares(transaction)
+      if (portfolio.shares === 0) await portfolio.destroy()
+      else await portfolio.save()
+      await user.save()
+      await transaction.save()
+      res.status(201).send(transaction)
+    } catch (err) {
+      next(err)
+    }
+  } else {
+    res.status(403).send('Forbidden')
   }
 })
---
+++ server/db/models/portfolio.spec.js
@@ -0,0 +1,56 @@
+const {expect} = require('chai')
+const db = require('../index')
+const Portfolio = db.model('portfolio')
+const Transaction = db.model('transaction')
+const User = db.model('user')
+
+describe('Portfolio model', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  describe('instance methods', () => {
+    let cody
+    let codyTransaction
+    let codyTransaction2
+
+    beforeEach(async () => {
+      cody = await User.create({
+        email: 'cody@puppybook.com',
+        password: 'bones'
+      })
+      codyTransaction = await Transaction.create({
+        type: 'purchase',
+        symbol: 'AAPL',
+        shares: 100,
+        pricePerShare: 132.05
+      })
+      codyTransaction2 = await Transaction.create({
+        type: 'sale',
+        symbol: 'AAPL',
+        shares: 50,
+        pricePerShare: 132.05
+      })
+      codyTransaction.setUser(cody)
+      codyTransaction2.setUser(cody)
+      cody.updateBalance(codyTransaction)
+    })
+    describe('updateShares', () => {
+      let applPortfolio
+      beforeEach(async () => {
+        let applPortfolioArray = await Portfolio.findOrCreate({
+          where: {symbol: codyTransaction.symbol, userId: cody.id}
+        })
+        applPortfolio = applPortfolioArray[0]
+        applPortfolio.updateShares(codyTransaction)
+      })
+      it('adds shares to applPortfolio', () => {
+        expect(applPortfolio.shares).to.equal(100)
+      })
+      it('removes shares on sale types', () => {
+          applPortfolio.updateShares(codyTransaction2)
+          expect(applPortfolio.shares).to.equal(50)
+      })
+    })
+  })
+})
