787f5d01f28da68c1417aee8724430ff732d1c1a
Richard Lanchez <richardsunny2015@gmail.com>
2018-10-25 20:06:35 -0400

    feat(portfolio): Instantiate Portfolio model (#19)

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,11 +1,15 @@
 const User = require('./user')
 const Transaction = require('./transaction')
+const Portfolio = require('./portfolio')
 
 /**
  * ASSOCIATIONS
  */
 Transaction.belongsTo(User)
 User.hasMany(Transaction)
+
+Portfolio.belongsTo(User)
+User.hasMany(Portfolio)
 
 module.exports = {
   User,
