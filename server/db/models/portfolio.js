617863598cc5d871c668ba92cecea8b93e19c94d
richardsunny2015 <richardsunny2015@gmail.com>
2018-10-25 20:05:48 -0400

    feat(portfolio): Instantiate Portfolio model

---
+++ server/db/models/portfolio.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Portfolio = db.define('portfolio', {
+    symbol: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    shares: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+})
+
+module.exports = Portfolio
