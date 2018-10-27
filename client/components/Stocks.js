8afec3387e933eb1cf743a50c35945598561c4ba
richardsunny2015 <richardsunny2015@gmail.com>
2018-10-27 09:15:15 -0400

    fix(Stocks): Dispatch me to retrieve new balance

--- client/components/Stocks.js
+++ client/components/Stocks.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import axios from 'axios'
+import {me} from '../store'
 
 const Stocks = props => {
-  const {stocks, userId, shares, balance} = props
+  const {stocks, userId, shares, balance, fetchBalance} = props
   const isPortfolio = props.isPortfolio || false
 
   return (
@@ -13,7 +14,7 @@ const Stocks = props => {
           <h4>{stock.symbol}</h4>
           <p>Current Price: {stock.price}</p>
           {isPortfolio && <p># of Shares: {shares[idx]}</p>}
-          <form onSubmit={purchaseSubmit(stock, userId, balance)}>
+          <form onSubmit={purchaseSubmit(stock, userId, balance, fetchBalance)}>
             <input type="number" name="shares" min="1" />
             <button type="submit" name="action" value="purchase">
               Buy
@@ -25,7 +26,7 @@ const Stocks = props => {
   )
 }
 
-const purchaseSubmit = (stockInfo, userId, balance) => evt => {
+const purchaseSubmit = (stockInfo, userId, balance, cb) => evt => {
   evt.preventDefault()
   const shares = Number(evt.target.shares.value)
   const total = stockInfo.price * shares
@@ -40,7 +41,7 @@ const purchaseSubmit = (stockInfo, userId, balance) => evt => {
     axios
       .post('/api/transactions', objToSend)
       .then(res => res.data)
-      .then(console.log)
+      .then(() => cb())
       .catch(console.log)
   } else {
     console.log('TOO MUCH')
@@ -53,4 +54,8 @@ const mapStateToProps = state => ({
   balance: state.user.balance
 })
 
-export default connect(mapStateToProps)(Stocks)
+const mapDispatchToProps = dispatch => ({
+  fetchBalance: () => dispatch(me())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Stocks)
